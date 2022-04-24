82657b1ca66891e54db920a1b65aff44f4c98e6f
anh.dev.2002@gmail.com <anhhvth2010043@fpt.edu.vn>
2022-04-24 23:10:56 +0700

    app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './components/layouts/Header';
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layouts/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return <>
+        <diiv>
+            Header
+        </diiv>
+    </>;
+}
+
+export default Header;
